0f551114452d5be0405b20015db564f41b17f890
jdrabot <jdrabot@hotmail.com>
2021-07-08 01:35:01 -0400

    created questions for all team members

--- index.js
+++ index.js
@@ -11,7 +11,83 @@ const questions = [
         type: "list",
         name: "teamMember",
         message: "What role does the employee play?",
-        choices: ["Manager", "Engineer", "Intern"],
+        choices: ["Manager", "Engineer", "Intern", "Finish"],
     },
 ];
+
+const managerQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is the manager's name?",
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the manager's id?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the manager's email?",
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the manager's office number?",
+    },
+];
+
+const engineerQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is the engineer's name?",
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the engineer's id?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the engineer's email?",
+    },
+    {
+        type: "input",
+        name: "githubUsername",
+        message: "What is the engineer's github username?",
+    },
+];
+
+const internQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is the intern's name?",
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the intern's id?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the intern's email?",
+    },
+    {
+        type: "input",
+        name: "school",
+        message: "Where did the intern go to school",
+    },
+];
+
+function manager() {
+    inquirer.prompt(managerQuestions).then((res) => {
+        const manag = new Manager(res.name, res.id, res.email, res.officeNumber);
+        myTeam.push(manag);
+    });
+}
 
